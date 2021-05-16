791e8ae765155edf68aa8af69e95c748c2a2e218
Sandeep-Workspace <chouhan.sandeep003@gmail.com>
2021-05-16 15:26:26 +0530

    AngularBootstrap and Angular Material Basic

---
+++ Angular/AngularBootstrap/src/app/app.module.ts
@@ -0,0 +1,25 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import {MatButtonModule} from '@angular/material/button';
+import { UsersComponent } from './users/users.component';
+@NgModule({
+  declarations: [
+    AppComponent,
+    UsersComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    NgbModule,
+    BrowserAnimationsModule,
+    MatButtonModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
